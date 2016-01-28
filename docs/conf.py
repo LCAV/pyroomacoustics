da12a47d94b482df5b5557afb187f022612a28ef
Robin Scheibler <fakufaku@gmail.com>
2016-01-28 17:19:23 +0100

    numpydoc.numpydoc

--- docs/conf.py
+++ docs/conf.py
@@ -15,6 +15,30 @@
 import sys
 import os
 
+"""
+class Mock(object):
+    def __init__(self, *args, **kwargs):
+        pass
+
+    def __call__(self, *args, **kwargs):
+        return Mock()
+
+    @classmethod
+    def __getattr__(self, name):
+        if name in ('__file__', '__path__'):
+            return '/dev/null'
+        elif name[0] == name[0].upper():
+            return type(name, (), {})
+        else:
+            return Mock()
+
+MOCK_MODULES = ['numpy', 'scipy', 'scipy.interpolate', 'romb', 'romberg', 
+                'matplotlib', 'tables']
+for mod_name in MOCK_MODULES:
+    sys.modules[mod_name] = Mock()
+"""
+
+
 # If extensions (or modules to document with autodoc) are in another directory,
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.
@@ -30,10 +54,10 @@ import os
 # ones.
 extensions = [
     'sphinx.ext.autodoc',
-    'numpydoc',
     'sphinx.ext.doctest',
     'sphinx.ext.coverage',
     'sphinx.ext.mathjax',
+    'numpydoc.numpydoc',
 ]
 
 # Add any paths that contain templates here, relative to this directory.
