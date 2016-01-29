109b376d479130060f0436706da748414d0ad98c
Robin Scheibler <fakufaku@gmail.com>
2016-01-29 16:50:07 +0100

    Trying to get do to work.

--- docs/conf.py
+++ docs/conf.py
@@ -57,7 +57,7 @@ extensions = [
     'sphinx.ext.doctest',
     'sphinx.ext.coverage',
     'sphinx.ext.mathjax',
-    'numpydoc.numpydoc',
+    'numpydoc',
 ]
 
 # Add any paths that contain templates here, relative to this directory.
