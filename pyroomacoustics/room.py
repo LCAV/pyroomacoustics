Optimization of isObstructed. Corrected bug in computation of wall normal in 3D for arbitrary polygons.

pyroomacoustics/room.py
@@ -712,7 +712,7 @@ class Room(object):
             genWallId = int(source.walls[imageId])
 
             # compute the location of the reflection on the wall
-            intersection = self.walls[genWallId].intersection(p, np.array(source.images[:, imageId]))
+            intersection = self.walls[genWallId].intersection(p, np.array(source.images[:, imageId]))[0]
 
             # the reflection point needs to be visible from the image source that generates the ray
             if intersection is not None:
@@ -752,8 +752,10 @@ class Room(object):
             
                 # Test if the line segment intersects the current wall
                 # We ignore intersections at boundaries of the line of sight
-                intersects, borderOfWall, borderOfSegment = self.walls[wallId].intersects(source.images[:, imageId], p)
-                if (intersects and not borderOfSegment):
+                #intersects, borderOfWall, borderOfSegment = self.walls[wallId].intersects(source.images[:, imageId], p)
+                intersectionPoint, borderOfSegment, borderOfWall = self.walls[wallId].intersection(source.images[:, imageId], p)
+
+                if (intersectionPoint is not None and not borderOfSegment):
                     
                     # Only images with order > 0 have a generating wall. At this point, there is obstruction for source order 0.
                     if (source.orders[imageId] > 0):
@@ -762,7 +764,7 @@ class Room(object):
                         # opposite sides of the generating wall 
                         # We ignore the obstruction if it is inside the
                         # generating wall (it is what happens in a corner)
-                        intersectionPoint = self.walls[wallId].intersection(source.images[:, imageId], p)
+                        #intersectionPoint = self.walls[wallId].intersection(source.images[:, imageId], p)
                         intersectionPointSide = self.walls[genWallId].side(intersectionPoint)
                         if (intersectionPointSide != imageSide and intersectionPointSide != 0):
                             return True

pyroomacoustics/wall.py
@@ -32,7 +32,13 @@ class Wall(object):
             self.normal = np.array([(self.corners[:, 1] - self.corners[:, 0])[1], (-1)*(self.corners[:, 1] - self.corners[:, 0])[0]])
             self.dim = 2
         elif (self.corners.shape[0] == 3 and self.corners[0].shape[0] > 2):
-            self.normal = np.cross(self.corners[:, 1] - self.corners[:, 0], self.corners[:, 2] - self.corners[:, 0])
+            # compute the normal assuming the vertices are aranged counter
+            # clock wise when the normal defines "up"
+            i_min = np.argmin(self.corners[0,:])
+            i_prev = i_min - 1 if i_min > 0 else self.corners.shape[1] - 1
+            i_next = i_min + 1 if i_min < self.corners.shape[1] - 1 else 0
+            self.normal = np.cross(self.corners[:, i_next] - self.corners[:, i_min], 
+                                   self.corners[:, i_prev] - self.corners[:, i_min])
             self.dim = 3
 
             # Compute a basis for the plane and project the corners into that basis
@@ -66,10 +72,10 @@ class Wall(object):
         p2 = np.array(p2)
     
         if (self.dim == 2):
-            return geom.intersection2DSegments(self.corners[:,0], self.corners[:,1], p1, p2)[0]
+            return geom.intersection2DSegments(p1, p2, self.corners[:,0], self.corners[:,1])
             
         if (self.dim == 3):
-            return geom.intersectionSegmentPolygonSurface(p1, p2, self.corners_2d, self.normal, self.plane_point, self.plane_basis)[0]
+            return geom.intersectionSegmentPolygonSurface(p1, p2, self.corners_2d, self.normal, self.plane_point, self.plane_basis)
         
     def intersects(self, p1, p2):
         """
@@ -85,7 +91,7 @@ class Wall(object):
         """
         
         if (self.dim == 2):
-            intersection, borderOfWall, borderOfSegment = geom.intersection2DSegments(self.corners[:,0], self.corners[:,1], p1, p2)
+            intersection, borderOfSegment, borderOfWall = geom.intersection2DSegments(p1, p2, self.corners[:,0], self.corners[:,1])
 
         if (self.dim == 3):
             intersection, borderOfSegment, borderOfWall = geom.intersectionSegmentPolygonSurface(p1, p2, self.corners_2d, self.normal,