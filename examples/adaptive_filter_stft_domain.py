bd97ca16c709ca7c9e1f0c3b367dfdd21c687d42
Eric B <eric.bezzam@epfl.ch>
2017-10-26 18:29:53 +0200

    Fixed error in computing output.

--- examples/adaptive_filter_stft_domain.py
+++ examples/adaptive_filter_stft_domain.py
@@ -14,9 +14,9 @@ import pyroomacoustics as pra
 
 # parameters
 num_taps = 6       # number of taps in frequency domain
-n_samples = 20000   # the number of samples to run
+n_samples = 40000   # the number of samples to run
 fft_length = 128   # block size
-SNR = 15           # signal to noise ratio
+SNR = 40           # signal to noise ratio (dB)
 fs = 16000
 
 """
@@ -29,47 +29,75 @@ W = np.random.randn(num_taps,num_bands) + \
     1j*np.random.randn(num_taps,num_bands)
 W /= np.linalg.norm(W, axis=0)
 
-# create a known driving signal
-x = np.random.randn(n_samples)
-
-# take to STFT domain
+# STFT blocks
 window = pra.hann(fft_length)  # the analysis window
 hop = fft_length//2
 stft_in = pra.realtime.STFT(fft_length, hop=hop, 
     analysis_window=window, channels=1)
 stft_out = pra.realtime.STFT(fft_length, hop=hop, 
     analysis_window=window, channels=1)
+num_blocks = n_samples//hop
 
-n = 0
-num_blocks = 0
-X_concat = np.zeros((num_bands,n_samples//hop),dtype=np.complex64)
-while  n_samples - n > hop:
+# create a known driving signal (reference) in time and frequency
+x = np.random.randn(n_samples)
 
-    stft_in.analysis(x[n:n+hop,])
-    X_concat[:,num_blocks] = stft_in.X
+samp = 0
+X_concat = np.zeros((num_bands,num_blocks),dtype=np.complex64)
+for n in range(num_blocks):
 
-    n += hop
-    num_blocks += 1
+    stft_in.analysis(x[samp:samp+hop,])
+    X_concat[:,n] = stft_in.X
+
+    samp += hop
+stft_in.reset()
 
 # convolve in frequency domain with unknown filter
 Y_concat = np.zeros((num_bands,num_blocks), dtype=np.complex64)
 for k in range(num_bands):
     Y_concat[k,:] = fftconvolve(X_concat[k,:], W[:,k])[:num_blocks]
 
-# add noise (is this right way?)
-V = np.random.randn(num_bands,num_blocks) + \
-    1j*np.random.randn(num_bands,num_blocks)
-V /= np.linalg.norm(V, axis=0) * np.linalg.norm(Y_concat, axis=0)
-V *= 10**(-SNR / 20.)
-D_concat = Y_concat + V
+# get time domain version
+y_true = np.zeros(n_samples)
+samp = 0
+for n in range(num_blocks):
+
+    y_true[samp:samp+hop,] = stft_out.synthesis(Y_concat[:,n])
+    samp += hop
+stft_out.reset()
+
+# create noise
+v = np.random.randn(n_samples) * 10**(-SNR / 20.)
+
+samp = 0
+V_concat = np.zeros((num_bands,num_blocks),dtype=np.complex64)
+for n in range(num_blocks):
+
+    stft_in.analysis(v[samp:samp+hop,])
+    V_concat[:,n] = stft_in.X
+
+    samp += hop
+stft_in.reset()
+
+# add noise
+D_concat = Y_concat + V_concat
+
+# V = np.random.randn(num_bands,num_blocks) + 
+#     1j*np.random.randn(num_bands,num_blocks)
+# V /= np.linalg.norm(V, axis=0) * np.linalg.norm(Y_concat, axis=0)
+# V *= 10**(-SNR / 20.)
+# D_concat = Y_concat + V.astype(np.complex64)
+
 
 # apply subband LMS
 adaptive_filters = pra.adaptive.SubbandLMS(num_taps=num_taps, 
     num_bands=num_bands, mu=0.5, nlms=True)
 
 y_hat = np.zeros(n_samples)
-aec_out = np.zeros(n_samples)
-error_per_band = np.zeros((num_bands,num_blocks), dtype=np.complex64)
+aec_out_time = np.zeros(n_samples)
+error_per_band = np.zeros((num_bands,num_blocks), dtype=np.float32)
+
+samp = 0
+stft_out.reset()
 for n in range(num_blocks):
 
     # update filter with new samples
@@ -77,12 +105,13 @@ for n in range(num_blocks):
     error_per_band[:,n] = np.linalg.norm(adaptive_filters.W.conj() - W, axis=0)
 
     # back to time domain
-    y_hat[n:n+hop,] = stft_in.synthesis(
+    y_hat[samp:samp+hop,] = stft_in.synthesis(
         np.diag(np.dot(adaptive_filters.W.conj().T,
         adaptive_filters.X)))
-    aec_out[n:n+hop,] = stft_out.synthesis(D_concat[:,n]-
-        np.diag(np.dot(adaptive_filters.W.conj().T,
-        adaptive_filters.X)))
+    aec_out_freq = D_concat[:,n]-np.diag(np.dot(adaptive_filters.W.conj().T,adaptive_filters.X))
+    aec_out_time[samp:samp+hop,] = stft_out.synthesis(aec_out_freq)
+
+    samp += hop
 
 # visualization and debug
 plt.figure()
@@ -95,4 +124,22 @@ plt.autoscale(enable=True, axis='x', tight=True)
 plt.xlabel('Time [s]')
 plt.ylabel('Filter error')
 plt.show()
+
+# # visualize in time domain
+# time_scale = np.arange(n_samples,dtype=np.float32)/fs
+# plt.figure()
+# plt.plot(time_scale, x)
+# plt.plot(time_scale, v)
+# plt.plot(time_scale, aec_out_time)
+# plt.autoscale(enable=True, axis='x', tight=True)
+# plt.xlabel('Time [s]')
+# plt.show()
+
+# # visualize in time domain
+# plt.figure()
+# plt.plot(time_scale, y_true)
+# plt.plot(time_scale, y_hat)
+# plt.autoscale(enable=True, axis='x', tight=True)
+# plt.xlabel('Time [s]')
+# plt.show()
 
