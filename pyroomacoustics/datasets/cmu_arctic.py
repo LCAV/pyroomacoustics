cce179708ee62746032e6b8077935066cbadebb5
Robin Scheibler <fakufaku@gmail.com>
2018-02-28 04:04:37 +0900

    Added facilities to slice down the data in cmu arctic corpus

--- pyroomacoustics/datasets/cmu_arctic.py
+++ pyroomacoustics/datasets/cmu_arctic.py
@@ -2,13 +2,26 @@
 The CMU ARCTIC Dataset
 ======================
 
-The CMU_ARCTIC databases were constructed at the Language Technologies Institute at Carnegie Mellon University as phonetically balanced, US English single speaker databases designed for unit selection speech synthesis research. A detailed report on the structure and content of the database and the recording environment etc is available as a Carnegie Mellon University, Language Technologies Institute Tech Report CMU-LTI-03-177 and is also available here.
+The CMU_ARCTIC databases were constructed at the Language Technologies
+Institute at Carnegie Mellon University as phonetically balanced, US English
+single speaker databases designed for unit selection speech synthesis research.
+A detailed report on the structure and content of the database and the
+recording environment etc is available as a Carnegie Mellon University,
+Language Technologies Institute Tech Report CMU-LTI-03-177 and is also
+available here.
 
-The databases consist of around 1150 utterances carefully selected from out-of-copyright texts from Project Gutenberg. The databses include US English male (bdl) and female (slt) speakers (both experinced voice talent) as well as other accented speakers.
+The databases consist of around 1150 utterances carefully selected from
+out-of-copyright texts from Project Gutenberg. The databses include US English
+male (bdl) and female (slt) speakers (both experinced voice talent) as well as
+other accented speakers.
 
 The 1132 sentence prompt list is available from cmuarctic.data
 
-The distributions include 16KHz waveform and simultaneous EGG signals. Full phoentically labelling was perfromed by the CMU Sphinx using the FestVox based labelling scripts. Complete runnable Festival Voices are included with the database distributions, as examples though better voices can be made by improving labelling etc.
+The distributions include 16KHz waveform and simultaneous EGG signals. Full
+phoentically labelling was perfromed by the CMU Sphinx using the FestVox based
+labelling scripts. Complete runnable Festival Voices are included with the
+database distributions, as examples though better voices can be made by
+improving labelling etc.
 
 License: Permissive, attribution required
 
@@ -66,6 +79,17 @@ class CMUArcticCorpus(object):
     This class will load the CMU ARCTIC corpus in a
     structure amenable to be processed.
 
+    Attributes
+    ----------
+    basedir: str, option
+        The directory where the CMU ARCTIC corpus is located/downloaded. By
+        default, this is the current directory.
+    speakers: list of str, optional
+        A list of the CMU ARCTIC speakers labels. If provided, only
+        those speakers are loaded. By default, all speakers are loaded.
+    sentences: list of CMUArcticSentence
+        The list of all utterances in the corpus
+
     Parameters
     ----------
     basedir: str, optional
@@ -79,7 +103,7 @@ class CMUArcticCorpus(object):
     '''
 
 
-    def __init__(self, basedir=None, download=False, speakers=None, cache=False):
+    def __init__(self, basedir=None, download=False, speakers=None, build=True):
 
         # default base directory is the current one
         self.basedir = basedir
@@ -89,11 +113,46 @@ class CMUArcticCorpus(object):
         # by default use all speakers
         self.speakers = speakers
         if self.speakers is None:
-            self.speakers = cmu_arctic_speakers.keys()
+            self.speakers = list(cmu_arctic_speakers.keys())
 
         # this is where the speakers directories should be
         self.basedir = os.path.join(self.basedir, corpus_dir)
+
+        # this should contain all the sentences
+        self.sentences = []
         
+        if build:
+            self.build_corpus()
+
+    def build_corpus(self, sex=None, lang=None, accent=None):
+        '''
+        Build the corpus with some filters (sex, lang, accent)
+        '''
+
+        # make sure these are lists
+        if sex is not None and not isinstance(sex, list):
+            sex = [sex]
+
+        if lang is not None and not isinstance(lang, list):
+            lang = [lang]
+
+        if accent is not None and not isinstance(accent, list):
+            accent = [accent]
+
+        # the matching formula
+        def match(s):
+            return (
+                    (sex is None or s['sex'] in sex)
+                    and (lang is None or s['lang'] in lang)
+                    and (accent is None or s['accent'] in accent)
+                    )
+
+        # first, we need to remove the speakers that do not match the filters
+        for spkr in self.speakers:
+            speaker = cmu_arctic_speakers[spkr]
+            if not match(speaker):
+                self.speakers.remove(spkr)
+
         # check everything requested is there or download
         if not os.path.exists(self.basedir):
             if download:
@@ -114,7 +173,6 @@ class CMUArcticCorpus(object):
                 else:
                     raise ValueError('Missing speaker directory. Please download.')
 
-        self.sentences = []
         for speaker in self.speakers:
             # Now crawl the directory to get all the data
             sdir = os.path.join(self.basedir, speaker_dir.format(speaker))
@@ -130,6 +188,75 @@ class CMUArcticCorpus(object):
                     text = line.split('"')[1]
 
                     self.sentences.append(CMUArcticSentence(path, tag, text, speaker, cmu_arctic_speakers[speaker]))
+
+    def filter(self, sex=None, lang=None, accent=None, speakers=None):
+        '''
+        Filter the corpus and selects sentences that match the criterias provided
+
+        The criterias can be strings or list of strings, for the latter any string
+        in the list is matched. If speakers are not specified, then all the speakers
+        are used.
+        '''
+
+        # make sure these are lists
+        if sex is not None and not isinstance(sex, list):
+            sex = [sex]
+
+        if lang is not None and not isinstance(lang, list):
+            lang = [lang]
+
+        if accent is not None and not isinstance(accent, list):
+            accent = [accent]
+
+        if speakers is None:
+            speakers = self.speakers
+        elif not isinstance(speakers, list):
+            speakers = [speakers]
+
+        # first, create the new empty corpus
+        new_corpus = CMUArcticCorpus(speakers=[], basedir=self.basedir, build=False)
+
+        # the matching formula
+        new_speakers = set()
+        def match(s):
+            q = ( (sex is None or s.sex in sex)
+                    and (lang is None or s.lang in lang)
+                    and (accent is None or s.accent in accent)
+                    and (s.speaker in speakers) )
+            # sneaky keep track of the speakers
+            if q:
+                new_speakers.add(s.speaker)
+            return q
+
+        # finally, add all the sentences
+        new_corpus.sentences = list(filter(match, self.sentences))
+
+        # finally update the list of speakers
+        new_corpus.speakers = list(new_speakers)
+
+        return new_corpus
+
+    def info(self):
+
+        attr = ['sex', 'lang', 'accent', 'speaker']
+
+        info = {}
+        for a in attr:
+            info[a] = set()
+
+        for s in self.sentences:
+            for a in attr:
+                info[a].add(s.__getattribute__(a))
+
+        return info
+        
+    def __getitem__(self, r):
+        return self.sentences[r]
+
+    def __len__(self):
+        return len(self.sentences)
+        
+
 
 
 
@@ -156,8 +283,10 @@ class CMUArcticSentence(object):
         Speaker initials
     sex: str
         Speaker gender (M or F)
-    dialect: str
-        Speaker dialect region number:
+    lang: str
+        Speaker language region number:
+    accent: str
+        Speaker accent
     fs: int
         sampling frequency
     samples: array_like (n_samples,)
@@ -179,7 +308,8 @@ class CMUArcticSentence(object):
         # extract the attributes
         self.tag = tag
         self.text = text
-        self.dialect = speaker_info['lang'] + '/' + speaker_info['accent']
+        self.lang = speaker_info['lang']
+        self.accent = speaker_info['accent']
         self.sex = speaker_info['sex']
         self.speaker = speaker
 
@@ -189,7 +319,7 @@ class CMUArcticSentence(object):
 
 
     def __str__(self):
-        s = " ".join([self.dialect, self.sex, self.speaker, self.text])
+        s = '{} ({}, {}/{}): ''{}'''.format(self.speaker, self.sex, self.lang, self.accent, self.text)
         return s
 
     def play(self):
