22ede2f60bdc788e54b44294b71453223d4433da
Eric B <eric.bezzam@epfl.ch>
2018-05-01 19:25:54 +0200

    Removed files not in upstream.

--- pyroomacoustics/datasets/__init__.py
+++ pyroomacoustics/datasets/__init__.py
@@ -107,4 +107,3 @@ Example 2 (CMU ARCTIC)
 from .base import Meta, Sample, AudioSample, Dataset
 from .timit import Word, Sentence, TimitCorpus
 from .cmu_arctic import CMUArcticCorpus, CMUArcticSentence, cmu_arctic_speakers
-from .google_speech_commands import GoogleSpeechCommands
