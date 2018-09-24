5665486eb493553ceaa512689b89f3e43dde9297
juan <jazcarretao@gmail.com>
2018-09-24 12:11:28 +0200

    Add ILRMA and integrate into BSS example

--- examples/bss_example.py
+++ examples/bss_example.py
@@ -1,23 +1,37 @@
 '''
-Blind Source Separation with Indpendent Vector Analysis
+Blind Source Separation offline example
 =======================================================
 
-Demonstrate how to do blind source separation (BSS) using the indpendent vector
-analysis technique. The method implemented is described in the following
-publication.
+Demonstrate the performance of different blind source separation (BSS) algorithms:
+
+1) Independent Vector Analysis (IVA)
+The method implemented is described in the following publication.
 
     N. Ono, *Stable and fast update rules for independent vector analysis based
     on auxiliary function technique*, Proc. IEEE, WASPAA, 2011.
 
-It works in the STFT domain. The test files were extracted from the
+2) Independent Low-Rank Matrix Analysis (ILRMA)
+The method implemented is described in the following publications
+
+    D. Kitamura, N. Ono, H. Sawada, H. Kameoka, H. Saruwatari, *Determined blind
+    source separation unifying independent vector analysis and nonnegative matrix
+    factorization,* IEEE/ACM Trans. ASLP, vol. 24, no. 9, pp. 1626-1641, September 2016
+
+    D. Kitamura, N. Ono, H. Sawada, H. Kameoka, and H. Saruwatari *Determined Blind
+    Source Separation with Independent Low-Rank Matrix Analysis*, in Audio Source Separation,
+    S. Makino, Ed. Springer, 2018, pp.  125-156.
+
+Both algorithms work in the STFT domain. The test files were extracted from the
 `CMU ARCTIC <http://www.festvox.org/cmu_arctic/>`_ corpus.
 
-Running this script will do two things.
+Depending on the input arguments running this script will do these actions:.
 
-1. It will separate the sources.
+1. Separate the sources.
 2. Show a plot of the clean and separated spectrograms
 3. Show a plot of the SDR and SIR as a function of the number of iterations.
 4. Create a `play(ch)` function that can be used to play the `ch` source (if you are in ipython say).
+5. Save the separated sources as .wav files
+6. Show a GUI where a mixed signals and the separated sources can be played
 
 This script requires the `mir_eval` to run, and `tkinter` and `sounddevice` packages for the GUI option.
 '''
@@ -40,14 +54,15 @@ wav_files = [
 if __name__ == '__main__':
 
     import argparse
-    parser = argparse.ArgumentParser(description='Demonstration of blind source separation using IVA.')
+    parser = argparse.ArgumentParser(description='Demonstration of blind source separation using IVA or ILRMA.')
     parser.add_argument('-b', '--block', type=int, default=2048,
             help='STFT block size')
+    parser.add_argument('-a', '--algo', type=str, default='ilrma',
+            help='Chooses BSS method to run: auxiva or ilrma')
     parser.add_argument('--gui', action='store_true',
             help='Creates a small GUI for easy playback of the sound samples')
     parser.add_argument('--save', action='store_true',
             help='Saves the output of the separation to wav files')
-
     args = parser.parse_args()
 
     if args.gui:
@@ -56,7 +71,6 @@ if __name__ == '__main__':
         matplotlib.use('TkAgg')
 
     import pyroomacoustics as pra
-
 
     # STFT frame length
     L = args.block
@@ -129,14 +143,20 @@ if __name__ == '__main__':
     ###########
     # The STFT needs front *and* back padding
 
-    # shape == (n_chan, n_frames, n_freq)
     X = np.array([pra.stft(ch, L, L, transform=np.fft.rfft, zp_front=L//2, zp_back=L//2) for ch in mics_signals])
     X = np.moveaxis(X, 0, 2)
 
-    # Run AuxIVA
-    Y = pra.bss.auxiva(X, n_iter=30, proj_back=True, callback=convergence_callback)
+    # Run BSS
+    bss_type = args.algo
+    if bss_type == 'auxiva':
+        # Run AuxIVA
+        Y = pra.bss.auxiva(X, n_iter=30, proj_back=True, callback=convergence_callback)
+    elif bss_type == 'ilrma':
+        # Run ILRMA
+        Y = pra.bss.ilrma(X, n_iter=30, n_components=30, proj_back=True,
+            callback=convergence_callback)
 
-    # run iSTFT
+    # Run iSTFT
     y = np.array([pra.istft(Y[:,:,ch], L, L, transform=np.fft.irfft, zp_front=L//2, zp_back=L//2) for ch in range(Y.shape[2])])
 
     # Compare SIR
--- pyroomacoustics/bss/__init__.py
+++ pyroomacoustics/bss/__init__.py
@@ -10,6 +10,9 @@ AuxIVA
 Trinicon
     | Time-domain BSS [2]_
     | :py:mod:`pyroomacoustics.bss.trinicon`
+ILRMA
+    | Independent Low-Rank Matrix Analysis [3]
+    | :py:mod:`pyroomacoustics.bss.ilrma`
 
 A few commonly used functions, such as projection back, can be found in
 :py:mod:`pyroomacoustics.bss.common`.
@@ -25,8 +28,13 @@ References
     acoustic environments*,  Signal Processing, 86(6), 1260-1277.
     doi:10.1016/j.sigpro.2005.06.022, 2006.
 
+.. [3] D. Kitamura, N. Ono, H. Sawada, H. Kameoka, and H. Saruwatari
+     *Determined Blind Source Separation with Independent Low-Rank Matrix Analysis*,
+     in Audio Source Separation, S. Makino, Ed. Springer, 2018, pp.  125-156.
+
 '''
 
 from .trinicon import trinicon
 from .auxiva import auxiva, f_contrasts
+from .ilrma import ilrma
 from .common import projection_back
