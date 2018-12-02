c9358b96994d3c450a0713f4fe234f9655edbb3f
Virgile Hernicot <virgile.hernicot@epfl.ch>
2018-12-02 09:31:51 +0100

    line 70 seems to be the problem

--- pyroomacoustics/bss/seer/sparir.py
+++ pyroomacoustics/bss/seer/sparir.py
@@ -3,8 +3,7 @@ from scipy.signal import resample
 
 
 def SpaRIR(G, S, delay=20, weights=np.array([]), q=1, gini=0):
-    L = G.shape[0] # n_freq
-    print(G.shape)
+    L = G.shape[0]  # n_freq
 
     if np.mod(L, 2) == 1:
         print('The length of G must be even.')
@@ -18,26 +17,24 @@ def SpaRIR(G, S, delay=20, weights=np.array([]), q=1, gini=0):
         S = np.array([S, (q * L - L / 2 - 1, 1) == 0])
         L = q * L
 
-    y = np.concatenate((np.real(G[S]), np.imag(G[S])),axis=0)
+    y = np.concatenate((np.real(G[S]), np.imag(G[S])), axis=0)
     M = y.shape[0]
-
 
     delay = q * delay
 
     if gini == 0:  # if no initialization is given
         g = np.zeros((L, 1))
-        g[delay + 1] = 1
+        g[delay] = 1
     else:
         g = gini
 
     if weights == 0:
-        tau = np.sqrt(L) / (y.T.dot(y))  # * ones(L,1)
+        tau = np.sqrt(L) / (y.conj().T.dot(y))  # * ones(L,1)
         tau = tau * np.exp(0.11 * np.abs((np.arange(1., L + 1.).T - delay) / q) ** 0.3)
     elif weights.size == 0:
-        tau = np.sqrt(L) / (y.T @ y)
+        tau = np.sqrt(L) / (y.conj().T.dot(y))
         tau = tau * np.exp(0.11 * np.abs((np.arange(1., L + 1.).T - delay) / q) ** 0.3)
         tau = tau.T
-        print(tau.shape)
     elif weights.shape[0] == 1:
         tau = np.ones((L, 1)) * weights
     else:
@@ -48,49 +45,47 @@ def SpaRIR(G, S, delay=20, weights=np.array([]), q=1, gini=0):
     alphamin = 1e-7  # minimum step - length parameteralpha
     tol = 1e-4
 
-    aux = np.zeros((L, 1))
+    aux = np.zeros((L, 1),dtype=complex)
     G = np.fft.fft(g.flatten())
-    Ag = np.concatenate((np.real(G[S]), np.imag(G[S])),axis=0)
+    Ag = np.concatenate((np.real(G[S]), np.imag(G[S])), axis=0)
     r = Ag - y.flatten()  # instead of r = A * g - y
-    aux[S] = np.expand_dims(r[0:M //2] + 1j * r[M//2:], axis=1)
+    aux[S] = np.expand_dims(r[0:M // 2] + 1j * r[M // 2:], axis=1)
     gradq = L / 2 * np.fft.irfft(aux.flatten(), L)  # instead of gradq = A'*r
     gradq = np.expand_dims(gradq, axis=1)
     alpha = 10
     support = g != 0
-    iter = 0
+    iter_ = 0
 
     crit = np.zeros((maxiter, 1))
 
     criterion = -tau[support] * np.sign(g[support]) - gradq[support]
-    crit[iter] = np.sum(criterion ** 2)
+    crit[iter_] = np.sum(criterion ** 2)
 
-    while (crit[iter] > tol) and (iter < maxiter):
-        print(iter)
+    while (crit[iter_] > tol) and (iter_ < maxiter):
+        print("iteration: ", iter_)
         prev_r = r
         prev_g = g
         g = soft(prev_g - gradq * (1 / alpha), tau / alpha)
-        print("g:", g)
         dg = g - prev_g
-
         DG = np.fft.rfft(dg)
-        Adg = np.concatenate((np.real(G[S]), np.imag(G[S])),axis=0)
-        r = prev_r + Adg  # faster than A * g - y
+        Adg = np.concatenate((np.real(DG[S]), np.imag(DG[S])), axis=0)
+        r = prev_r + Adg.flatten()  # faster than A * g - y
         dd = dg.flatten().conj().T @ dg.flatten()
-        dd = np.real(dd)
+        print(dd)
         dGd = Adg.flatten().conj().T @ Adg.flatten()
         alpha = min(alphamax, max(alphamin, dGd / (np.finfo(np.float32).eps + dd)))
         support = g != 0
-        aux[S] = np.expand_dims(r[0:M // 2] + 1j * r[M //2:], axis=1)
+        aux[S] = np.expand_dims(r[0:M // 2] + 1j * r[M // 2:], axis=1)
         gradq = L / 2 * np.fft.irfft(aux.flatten(), L)
         gradq = np.expand_dims(gradq, axis=1)
         criterion = -tau[support] * np.sign(g[support]) - gradq[support]
-        crit[iter] = sum(criterion ** 2) + sum(abs(gradq[~support]) - tau[~support] > tol)
-        iter = iter + 1
+        crit[iter_] = sum(criterion ** 2) + sum(abs(gradq[~support]) - tau[~support] > tol)
+        iter_ = iter_ + 1
 
     if q > 1:
         g = q * resample(g, 1, q, 100)
 
-    print('SpaRIR: {0} iterations done.'.format(iter))
+    print('SpaRIR: {0} iterations done.'.format(iter_))
 
     if sum(g == 0) == L:
         print('Zero solution! Do you really like it?')
@@ -101,7 +96,7 @@ def SpaRIR(G, S, delay=20, weights=np.array([]), q=1, gini=0):
 
 
 def soft(x, T):
-    if np.sum(np.abs(T)) == 0:
+    if np.sum(np.abs(T).flatten()) == 0:
         y = x
     else:
         y = np.max(np.abs(x) - T, 0)
