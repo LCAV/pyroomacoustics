42ebe6562afdc694e8df6e28df774f04c13ece5c
Robin Scheibler <fakufaku@gmail.com>
2018-12-13 16:38:15 +0900

    Adds a test that the same RIR with different constructor of Rooms

--- pyroomacoustics/tests/test_issue_69.py
+++ pyroomacoustics/tests/test_issue_69.py
@@ -1,6 +1,5 @@
 import unittest
 import numpy as np
-import matplotlib.pyplot as plt
 import pyroomacoustics as pra
 
 room_dim = [15, 14, 16]
@@ -8,7 +7,7 @@ absorption = 0.2
 source_position = [2.0, 3.1, 2.0]
 mic_position = [2.0, 1.5, 2.0]
 fs = 16000
-max_order = 15
+max_order = 5
 
 # scenario A
 def get_room_constructor_args():
@@ -28,6 +27,7 @@ def get_room_constructor_args():
             )
 
     shoebox.image_source_model()
+    shoebox.compute_rir()
     return shoebox
 
 #scenario B
@@ -38,6 +38,7 @@ def get_room_add_method():
     shoebox.add_microphone_array(mics)
 
     shoebox.image_source_model()
+    shoebox.compute_rir()
     return shoebox
 
 class RoomConstructorSources(unittest.TestCase):
@@ -50,6 +51,12 @@ class RoomConstructorSources(unittest.TestCase):
     def test_room_add_method(self):
         room_2 = get_room_add_method()
         self.assertTrue(isinstance(room_2.sources[0], pra.SoundSource))
+
+    def test_rir_equal(self):
+        room_1 = get_room_constructor_args()
+        room_2 = get_room_add_method()
+        self.assertTrue(np.allclose(room_1.rir[0][0], room_2.rir[0][0]))
+
 
 if __name__ == '__main__':
 
