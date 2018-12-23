Correcting the energy by averaging the scattered components. Modify all the tests accordingly.

examples/room_L_shape_3d_rt.py
@@ -17,9 +17,9 @@ import pyroomacoustics as pra
 from scipy.io import wavfile
 
 # Create the 2D L-shaped room from the floor polygon
-pol = 4 * np.array([[0,0], [0,1], [2,1], [2,0.5], [1,0.5], [1,0]]).T
-r_absor = 0.1
-room = pra.Room.from_corners(pol, fs=16000, max_order=6, absorption=r_absor)
+pol = 10 * np.array([[0,0], [0,1], [2,1], [2,0.5], [1,0.5], [1,0]]).T
+r_absor = 0.01
+room = pra.Room.from_corners(pol, fs=16000, max_order=9, absorption=r_absor)
 
 # Create the 3D room by extruding the 2D by 3 meters
 room.extrude(3., absorption=r_absor)
@@ -35,10 +35,10 @@ R = np.array([[3., 1.],
 
 room.add_microphone_array(pra.MicrophoneArray(R, room.fs))
 
-print("Volume : ", room.get_volume())
+# print("relative mic size : ", room.get_volume()/(4*np.pi*mic_radius*mic_radius*mic_radius/3))
 
 chrono = time.time()
-room.compute_rir(mode='hybrid', nb_thetas=300, nb_phis=300, scatter_coef=0, mic_radius=0.10)
+room.compute_rir(mode='ism', nb_thetas=300, nb_phis=300, scatter_coef=0.1, mic_radius=0.15)
 print("Done in", time.time()-chrono, "seconds.")
 
 

pyroomacoustics/libroom_src/tests/test_room_walls.py
@@ -292,11 +292,17 @@ class TestRoomWalls(unittest.TestCase):
         time_thres = 200.
 
         sound_speed = 340.
-        output = [[[1., 2.]]]  # arbitrary initialisation to have the correct shape
 
+        fs = 16000
 
-        self.assertTrue(not room.scat_ray(energy, scatter_coef, prev_wall, prev_last_hit, last_hit, radius, total_dist, travel_time,
-                              time_thres, energy_thres, sound_speed, False, output))
+        # The function will try to access elements from this array, so we must initialize it
+        # (even though we don't care of what the latter contains here)
+        scat_per_slot = [[0]*int(np.floor(time_thres*fs))]
+
+        output = [[[1.,2.,-1]]] #arbitrary initialisation to have the correct shape
+        self.assertTrue(not room.scat_ray(energy, scatter_coef, prev_wall, prev_last_hit, last_hit,
+                                      radius, total_dist, travel_time, time_thres, energy_thres,
+                                      sound_speed, False,fs, scat_per_slot, output))
 
 
     def test_scat_ray_ok(self):
@@ -329,8 +335,16 @@ class TestRoomWalls(unittest.TestCase):
 
         sound_speed = 340.
 
-        output = [[[1.,2.]]] #arbitrary initialisation to have the correct shape
-        self.assertTrue(room.scat_ray(energy, scatter_coef, prev_wall, prev_last_hit, last_hit, radius, total_dist, travel_time, time_thres, energy_thres, sound_speed, False, output))
+        fs = 16000
+
+        # The function will try to access elements from this array, so we must initialize it
+        # (even though we don't care of what the latter contains here)
+        scat_per_slot = [[0]*int(np.floor(time_thres*fs))]
+
+        output = [[[1.,2.,-1]]] #arbitrary initialisation to have the correct shape
+        self.assertTrue(room.scat_ray(energy, scatter_coef, prev_wall, prev_last_hit, last_hit,
+                                      radius, total_dist, travel_time, time_thres, energy_thres,
+                                      sound_speed, False,fs, scat_per_slot, output))
 
 
     def test_scat_ray_energy(self):
@@ -361,13 +375,11 @@ class TestRoomWalls(unittest.TestCase):
         energy = 1.
         scatter_coef = 0.1
 
-        eps = 0.00001
+        eps = 0.0001
 
         result = room.compute_scat_energy(energy, scatter_coef, prev_wall, prev_last_hit, last_hit, mic_pos, radius, total_dist, True)
-        print(result)
-
-
         self.assertTrue(np.allclose(result, 0.1/(4*np.pi*5), atol=eps))
+
 
     def test_contains_2D(self):
 