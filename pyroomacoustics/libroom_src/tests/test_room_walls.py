Removing the averaging over all scattered rays since the scattered 'amplitude' is now correct.

pyroomacoustics/libroom_src/tests/test_room_walls.py
@@ -293,16 +293,10 @@ class TestRoomWalls(unittest.TestCase):
 
         sound_speed = 340.
 
-        fs = 16000
-
-        # The function will try to access elements from this array, so we must initialize it
-        # (even though we don't care of what the latter contains here)
-        scat_per_slot = [[0]*int(np.floor(time_thres*fs))]
-
-        output = [[[1.,2.,-1]]] #arbitrary initialisation to have the correct shape
+        output = [[[1.,2.]]] #arbitrary initialisation to have the correct shape
         self.assertTrue(not room.scat_ray(energy, scatter_coef, prev_wall, prev_last_hit, last_hit,
                                       radius, total_dist, travel_time, time_thres, energy_thres,
-                                      sound_speed, False,fs, scat_per_slot, output))
+                                      sound_speed, False, output))
 
 
     def test_scat_ray_ok(self):
@@ -335,16 +329,10 @@ class TestRoomWalls(unittest.TestCase):
 
         sound_speed = 340.
 
-        fs = 16000
-
-        # The function will try to access elements from this array, so we must initialize it
-        # (even though we don't care of what the latter contains here)
-        scat_per_slot = [[0]*int(np.floor(time_thres*fs))]
-
-        output = [[[1.,2.,-1]]] #arbitrary initialisation to have the correct shape
+        output = [[[1.,2.]]] #arbitrary initialisation to have the correct shape
         self.assertTrue(room.scat_ray(energy, scatter_coef, prev_wall, prev_last_hit, last_hit,
                                       radius, total_dist, travel_time, time_thres, energy_thres,
-                                      sound_speed, False,fs, scat_per_slot, output))
+                                      sound_speed, False, output))
 
 
     def test_scat_ray_energy(self):