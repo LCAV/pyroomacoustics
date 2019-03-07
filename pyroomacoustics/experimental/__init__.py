4d033bfa0070156cd34732e0f0cb7339c65dad55
Robin Scheibler <fakufaku@gmail.com>
2019-03-07 13:59:36 +0900

    Added function to measure RT60

--- pyroomacoustics/experimental/__init__.py
+++ pyroomacoustics/experimental/__init__.py
@@ -5,12 +5,16 @@ Experimental
 A bunch of routines useful when doing measurements and experiments.
 '''
 
-__all__ = ['measure_ir', 'physics', 'point_cloud', 'delay_calibration', 'deconvolution', 'localization', 'signals']
+__all__ = [
+        'measure_ir', 'physics', 'point_cloud', 'delay_calibration',
+        'deconvolution', 'localization', 'signals',
+]
 
-from .measure_ir import *
-from .physics import *
-from .point_cloud import *
-from .delay_calibration import *
-from .deconvolution import *
-from .localization import *
-from .signals import *
+from .measure_ir import measure_ir
+from .physics import calculate_speed_of_sound
+from .point_cloud import PointCloud
+from .delay_calibration import DelayCalibration
+from .deconvolution import deconvolve, wiener_deconvolve
+from .localization import tdoa_loc, edm_line_search
+from .signals import exponential_sweep, linear_sweep
+from .rt60 import measure_rt60
