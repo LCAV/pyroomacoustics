2f8fa4c3a46ff90dd99a3ab2b16754b5b2cc8240
Robin Scheibler <fakufaku@gmail.com>
2020-01-21 13:38:03 +0900

    Fixes scat_ray test

--- pyroomacoustics/libroom_src/room.cpp
+++ pyroomacoustics/libroom_src/room.cpp
@@ -25,6 +25,7 @@
  */
 
 #include <iostream>
+#include <cmath>
 #include <algorithm>
 #include "room.hpp"
 
@@ -715,7 +716,7 @@ bool Room<D>::scat_ray(
       float p_hit_equal = 1.f - sqrt(1.f - m_sq / h_sq);
       // cosine angle should be positive, but could be negative if normal is
       // facing out of room so we take abs
-      float p_lambert = 2 * abs(wall.cosine_angle(hit_point_to_mic));
+      float p_lambert = 2 * std::abs(wall.cosine_angle(hit_point_to_mic));
       Eigen::VectorXf scat_trans = wall.scatter * transmitted * p_hit_equal * p_lambert;
 
       // We add an entry to output and we increment the right element
