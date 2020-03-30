99b66773d02976886d30c4c539f602c0608f6734
Robin Scheibler <fakufaku@gmail.com>
2020-03-30 21:50:43 +0900

    next_hit_wall test

--- pyroomacoustics/tests/tests_libroom/test_ray_energy.py
+++ pyroomacoustics/tests/tests_libroom/test_ray_energy.py
@@ -80,17 +80,25 @@ class TestRayEnergy(unittest.TestCase):
             transmitted *= (1.0 - energy_absorption) ** 4  # 4 wall hits
             distance += round_trip
 
-        print("Creating the python room")
-        room = pra.ShoeBox(
-            [2, 2, 2], fs=16000, materials=pra.Material(energy_absorption)
-        )
+        print("Creating the python room (polyhedral)")
+        walls_corners = [
+            np.array([[0, 2, 2, 0], [0, 0, 0, 0], [0, 0, 2, 2]]),  # left
+            np.array([[0, 0, 2, 2], [2, 2, 2, 2], [0, 2, 2, 0]]),  # right
+            np.array([[0, 0, 0, 0], [0, 2, 2, 0], [0, 0, 2, 2]]),  # front`
+            np.array([[2, 2, 2, 2], [0, 0, 2, 2], [0, 2, 2, 0]]),  # back
+            np.array([[0, 2, 2, 0], [0, 0, 2, 2], [0, 0, 0, 0],]),  # floor
+            np.array([[0, 0, 2, 2], [0, 2, 2, 0], [2, 2, 2, 2],]),  # ceiling
+        ]
+        walls = [pra.wall_factory(c, [energy_absorption], [0.0]) for c in walls_corners]
+        room_poly = pra.Room(walls, fs=16000)
         # room = pra.Room(walls, fs=16000)
-        room.add_source([0.5, 0.5, 1])
-        room.add_microphone_array(pra.MicrophoneArray(np.c_[[1.5, 1.5, 1.0]], room.fs))
+        room_poly.add_source([0.5, 0.5, 1])
+        room_poly.add_microphone_array(
+            pra.MicrophoneArray(np.c_[[1.5, 1.5, 1.0]], room_poly.fs)
+        )
 
-        print("Creating the cpp room")
-        room.room_engine.set_params(
-            room.c,
+        room_poly.room_engine.set_params(
+            room_poly.c,
             0,
             energy_thresh,  # energy threshold for rays
             5.0,  # time threshold for rays
@@ -99,15 +107,45 @@ class TestRayEnergy(unittest.TestCase):
             False,  # is it hybrid model ?
         )
 
-        print("Running ray tracing")
-        room.room_engine.ray_tracing(
-            np.c_[[-np.pi / 4.0, np.pi / 2.0]], room.sources[0].position  # source loc
+        print("Running ray tracing (polyhedral)")
+        room_poly.room_engine.ray_tracing(
+            np.c_[[-np.pi / 4.0, np.pi / 2.0]],
+            room_poly.sources[0].position,  # source loc
         )
 
-        h = room.room_engine.microphones[0].histograms[0].get_hist()
-        histogram_rt = np.array(h[0])[: len(histogram_gt)]
+        print("Creating the python room (shoebox)")
+        room_cube = pra.ShoeBox(
+            [2, 2, 2], fs=16000, materials=pra.Material(energy_absorption)
+        )
+        # room = pra.Room(walls, fs=16000)
+        room_cube.add_source([0.5, 0.5, 1])
+        room_cube.add_microphone_array(
+            pra.MicrophoneArray(np.c_[[1.5, 1.5, 1.0]], room_poly.fs)
+        )
 
-        self.assertTrue(np.allclose(histogram_rt, histogram_gt))
+        room_cube.room_engine.set_params(
+            room_poly.c,
+            0,
+            energy_thresh,  # energy threshold for rays
+            5.0,  # time threshold for rays
+            detector_radius,  # detector radius
+            hist_bin_size,  # resolution of histogram [s]
+            False,  # is it hybrid model ?
+        )
+
+        print("Running ray tracing (shoebox)")
+        room_cube.room_engine.ray_tracing(
+            np.c_[[-np.pi / 4.0, np.pi / 2.0]],
+            room_cube.sources[0].position,  # source loc
+        )
+
+        h_poly = room_poly.room_engine.microphones[0].histograms[0].get_hist()
+        h_cube = room_cube.room_engine.microphones[0].histograms[0].get_hist()
+        histogram_rt_poly = np.array(h_poly[0])[: len(histogram_gt)]
+        histogram_rt_cube = np.array(h_cube[0])[: len(histogram_gt)]
+
+        self.assertTrue(np.allclose(histogram_rt_poly, histogram_gt))
+        self.assertTrue(np.allclose(histogram_rt_cube, histogram_gt))
 
 
 if __name__ == "__main__":
