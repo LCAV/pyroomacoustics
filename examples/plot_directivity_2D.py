7be52fceed7ad68274a86759ba88287c0799c9a5
ebezzam <ebezzam@gmail.com>
2020-07-27 12:02:19 +0200

    Update examples for directivities.

--- examples/plot_directivity_2D.py
+++ examples/plot_directivity_2D.py
@@ -4,6 +4,7 @@ import matplotlib.pyplot as plt
 from pyroomacoustics import dB
 from pyroomacoustics.directivities import DirectivityPattern, \
     DirectionVector, CardioidFamily
+from pyroomacoustics.doa import spher2cart
 
 ORIENTATION = DirectionVector(azimuth=0, colatitude=90, degrees=True)
 LOWER_GAIN = -20
@@ -11,6 +12,7 @@ LOWER_GAIN = -20
 # plot each directivity
 angles = np.linspace(start=0, stop=360, num=361, endpoint=True)
 angles = np.radians(angles)
+cart = spher2cart(azimuth=angles)
 
 # plot each pattern
 fig = plt.figure()
@@ -18,12 +20,9 @@ ax = plt.subplot(111, projection="polar")
 for _dir in DirectivityPattern.values():
 
     dir_obj = CardioidFamily(orientation=ORIENTATION, pattern_name=_dir)
-    gains = []
-    for a in angles:
-        direction = DirectionVector(azimuth=a, degrees=False)
-        gains.append(dir_obj.get_response(direction))
-    gains_db = dB(np.array(gains))
-    ax.plot(angles, gains_db, label=_dir)
+    resp = dir_obj.get_response(coord=cart, magnitude=True)
+    resp_db = dB(np.array(resp))
+    ax.plot(angles, resp_db, label=_dir)
 
 plt.legend(bbox_to_anchor=(1, 1))
 plt.ylim([LOWER_GAIN, 0])
