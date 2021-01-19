Corrects bug in computation of SRP cost function (#199)
* Corrects a bug in the computation of the cost function of SRP
* Adds a test for the SRP cost function computation bug
* Simplify new implementation of SRP-PHAT. Removes commented lines

pyroomacoustics/doa/srp.py
@@ -4,6 +4,7 @@ from __future__ import division, print_function
 
 from .doa import *
 
+
 class SRP(DOA):
     """
     Class to apply Steered Response Power (SRP) direction-of-arrival (DoA) for 
@@ -36,23 +37,42 @@ class SRP(DOA):
         Candidate elevation angles (in radians) with respect to z-axis.
         Default is x-y plane search: np.pi/2*np.ones(1)
     """
-    def __init__(self, L, fs, nfft, c=343.0, num_src=1, mode='far', r=None, 
-        azimuth=None, colatitude=None, **kwargs):
 
-        DOA.__init__(self, L=L, fs=fs, nfft=nfft, c=c, num_src=num_src, 
-            mode=mode, r=r, azimuth=azimuth, colatitude=colatitude, **kwargs)
+    def __init__(
+        self,
+        L,
+        fs,
+        nfft,
+        c=343.0,
+        num_src=1,
+        mode="far",
+        r=None,
+        azimuth=None,
+        colatitude=None,
+        **kwargs
+    ):
 
-        self.num_pairs = self.M*(self.M-1)/2
+        DOA.__init__(
+            self,
+            L=L,
+            fs=fs,
+            nfft=nfft,
+            c=c,
+            num_src=num_src,
+            mode=mode,
+            r=r,
+            azimuth=azimuth,
+            colatitude=colatitude,
+            **kwargs
+        )
 
-        #self.mode_vec = np.conjugate(self.mode_vec)
+        self.num_pairs = self.M * (self.M - 1) / 2
 
     def _process(self, X):
         """
         Perform SRP-PHAT for given frame in order to estimate steered response 
         spectrum.
         """
-
-        ones = np.ones(self.L.shape[1])
 
         srp_cost = np.zeros(self.grid.n_points)
 
@@ -63,24 +83,54 @@ class SRP(DOA):
 
         CC = []
         for k in self.freq_bins:
-            CC.append( np.dot(pX[:,k,:], np.conj(pX[:,k,:]).T) )
+            CC.append(np.dot(pX[:, k, :], np.conj(pX[:, k, :]).T))
         CC = np.array(CC)
+
+        M = self.L.shape[1]
+        ar = np.arange(M)
+        av = ar[:, None]
+
+        # the mask here allow to select all the coefficients above
+        # the main diagonal of the covariance matrix
+        # It can be applied on the flattened last two dimensions of the
+        # stack of cov. matrices
+        mask_triu = (av < av.T).flatten()
+
+        # Flatten the covariance matrices and use the above mask to
+        # select the upper triangular part
+        CC_flat = CC.reshape((-1, CC.shape[-2] * CC.shape[-2]))[:, mask_triu]
+
+        # The DC offset is the sum of all the diagonal coefficients
+        # Due to the normalization in SRP-PHAT, they are all ones,
+        # and we end up with the product:
+        # <number of frames> x <number of microphones> x <number of freqs>
+        # the number of frames appears because the covariance matrix
+        # is not normalized
+        DC_offset = pX.shape[-1] * self.L.shape[1] * len(self.freq_bins)
 
         for n in range(self.grid.n_points):
 
+            # In the loop, this is just a fancy way of computing
+            # the quadratic form:
+            # mode_vec^H @ CC @ mode_vec
+
             # get the mode vector axis: (frequency, microphones)
-            mode_vec = self.mode_vec[self.freq_bins,:,n]
+            mode_vec = self.mode_vec[self.freq_bins, :, n]
 
             # compute the outer product along the microphone axis
-            mode_mat = np.conj(mode_vec[:,:,None]) * mode_vec[:,None,:]
+            mode_mat = np.conj(mode_vec[:, :, None]) * mode_vec[:, None, :]
 
-            # multiply covariance by mode vectors and sum over the frequencies
-            R = np.sum(CC * mode_mat, axis=0)
-
-            # Now sum over all distince microphone pairs
-            sum_val = np.inner(ones, np.dot(np.triu(R, 1), ones))
+            # First, we flatten the mode vector covariance and select the
+            # terms above the main diagonal
+            mode_mat_flat = mode_mat.reshape((-1, M * M))[:, mask_triu]
+            # Then we compute manually the real part of the element-wise product
+            # This is equivalent to `np.real(CC_flat * mode_mat_flat)
+            # but avoids computing the imaginary part that we end up discarding
+            R = CC_flat.real * mode_mat_flat.real - CC_flat.imag * mode_mat_flat.imag
+            # Finally, we sum up and add the DC offset to make the cost non-negative
+            sum_val = 2.0 * np.sum(R) + DC_offset
 
             # Finally normalize
-            srp_cost[n] = np.abs(sum_val) / self.num_snap/self.num_freq/self.num_pairs
+            srp_cost[n] = sum_val / self.num_snap / self.num_freq / self.num_pairs
 
         self.grid.set_values(srp_cost)

pyroomacoustics/doa/tests/test_doa.py
@@ -2,7 +2,8 @@
 # @author: robin.scheibler@epfl.ch, ivan.dokmanic@epfl.ch, sidney.barthe@epfl.ch
 # @copyright: EPFL-IC-LCAV 2017
 
-from unittest import TestCase
+import unittest
+
 import numpy as np
 from scipy.signal import fftconvolve
 
@@ -12,17 +13,17 @@ import pyroomacoustics as pra
 np.random.seed(0)
 
 # Location of original source
-azimuth = 61. / 180. * np.pi  # 60 degrees
-tol = 0.3 / 180. * np.pi  # 0.3 degrees tolerance for the test
+azimuth = 61.0 / 180.0 * np.pi  # 60 degrees
+tol = 0.3 / 180.0 * np.pi  # 0.3 degrees tolerance for the test
 
 # algorithms parameters
-c = 343.
+c = 343.0
 fs = 16000
 nfft = 256
 freq_bins = np.arange(5, 60)
 
 # circular microphone array, 12 mics, radius 15 cm
-R = pra.circular_2D_array([0, 0], 12, 0., 0.15)
+R = pra.circular_2D_array([0, 0], 12, 0.0, 0.15)
 
 # propagation filter bank
 propagation_vector = -np.array([np.cos(azimuth), np.sin(azimuth)])
@@ -34,54 +35,74 @@ x = np.random.randn((nfft // 2 + 1) * nfft)
 
 # convolve the source signal with the fractional delay filters
 # to get the microphone input signals
-mic_signals = np.array([ fftconvolve(x, filter, mode='same') for filter in filter_bank ])
+mic_signals = np.array([fftconvolve(x, filter, mode="same") for filter in filter_bank])
 X = pra.transform.analysis(mic_signals.T, nfft, nfft // 2, win=np.hanning(nfft))
 X = np.swapaxes(X, 2, 0)
 
-class TestDOA(TestCase):
 
+class TestDOA(unittest.TestCase):
     def test_music(self):
-        doa = pra.doa.algorithms['MUSIC'](R, fs, nfft, c=c)
+        doa = pra.doa.algorithms["MUSIC"](R, fs, nfft, c=c)
         doa.locate_sources(X, freq_bins=freq_bins)
-        print('distance:', pra.doa.circ_dist(azimuth, doa.azimuth_recon))
+        print("distance:", pra.doa.circ_dist(azimuth, doa.azimuth_recon))
         self.assertTrue(pra.doa.circ_dist(azimuth, doa.azimuth_recon) < tol)
 
     def test_srp_phat(self):
-        doa = pra.doa.algorithms['SRP'](R, fs, nfft, c=c)
+        doa = pra.doa.algorithms["SRP"](R, fs, nfft, c=c)
         doa.locate_sources(X, freq_bins=freq_bins)
-        print('distance:', pra.doa.circ_dist(azimuth, doa.azimuth_recon))
+        print("distance:", pra.doa.circ_dist(azimuth, doa.azimuth_recon))
         self.assertTrue(pra.doa.circ_dist(azimuth, doa.azimuth_recon) < tol)
 
+    def test_srp_phat_2ch(self):
+        """
+        Tests that the cost function of SRP is properly computed (PR #197)
+
+        This checks for the presence of a bug that was detected in PR #197.
+        """
+        doa = pra.doa.algorithms["SRP"](R[:, :2], fs, nfft, c=c)
+        i_freq = 10
+        doa.locate_sources(X[:2, :, :], freq_bins=freq_bins[i_freq : i_freq + 1])
+        std_val = np.std(doa.grid.values)
+        self.assertTrue(std_val > 1e-10)
+
     def test_cssm(self):
-        doa = pra.doa.algorithms['CSSM'](R, fs, nfft, c=c)
+        doa = pra.doa.algorithms["CSSM"](R, fs, nfft, c=c)
         doa.locate_sources(X, freq_bins=freq_bins)
-        print('distance:', pra.doa.circ_dist(azimuth, doa.azimuth_recon))
+        print("distance:", pra.doa.circ_dist(azimuth, doa.azimuth_recon))
         self.assertTrue(pra.doa.circ_dist(azimuth, doa.azimuth_recon) < tol)
 
     def test_tops(self):
-        doa = pra.doa.algorithms['TOPS'](R, fs, nfft, c=c)
+        doa = pra.doa.algorithms["TOPS"](R, fs, nfft, c=c)
         doa.locate_sources(X, freq_bins=freq_bins)
-        print('distance:', pra.doa.circ_dist(azimuth, doa.azimuth_recon))
+        print("distance:", pra.doa.circ_dist(azimuth, doa.azimuth_recon))
         self.assertTrue(pra.doa.circ_dist(azimuth, doa.azimuth_recon) < tol)
 
     def test_waves(self):
-        doa = pra.doa.algorithms['WAVES'](R, fs, nfft, c=c)
+        doa = pra.doa.algorithms["WAVES"](R, fs, nfft, c=c)
         doa.locate_sources(X, freq_bins=freq_bins)
-        print('distance:', pra.doa.circ_dist(azimuth, doa.azimuth_recon))
+        print("distance:", pra.doa.circ_dist(azimuth, doa.azimuth_recon))
         self.assertTrue(pra.doa.circ_dist(azimuth, doa.azimuth_recon) < tol)
 
     def test_frida(self):
-        doa = pra.doa.algorithms['FRIDA'](R, fs, nfft, c=c)
+        doa = pra.doa.algorithms["FRIDA"](R, fs, nfft, c=c)
         doa.locate_sources(X, freq_bins=freq_bins)
-        print('distance:', pra.doa.circ_dist(azimuth, doa.azimuth_recon))
+        print("distance:", pra.doa.circ_dist(azimuth, doa.azimuth_recon))
         self.assertTrue(pra.doa.circ_dist(azimuth, doa.azimuth_recon) < tol)
 
-if __name__ == '__main__':
 
+if __name__ == "__main__":
+
+    """
     algo_names = sorted(pra.doa.algorithms.keys())
 
     for algo_name in algo_names:
         doa = pra.doa.algorithms[algo_name](R, fs, nfft, c=c, max_four=4)
         doa.locate_sources(X, freq_bins=freq_bins)
-        print(algo_name, doa.azimuth_recon / np.pi * 180., 
-            pra.doa.circ_dist(azimuth, doa.azimuth_recon) / np.pi * 180.)
+        print(
+            algo_name,
+            doa.azimuth_recon / np.pi * 180.0,
+            pra.doa.circ_dist(azimuth, doa.azimuth_recon) / np.pi * 180.0,
+        )
+    """
+
+    unittest.main()