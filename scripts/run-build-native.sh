6ec5d2980c78a1f941de55704939634b1ae316f8
Robin Scheibler <fakufaku@gmail.com>
2021-04-08 14:07:58 +0900

    Modify build scripts for arm

--- scripts/run-build-native.sh
+++ scripts/run-build-native.sh
@@ -1,6 +1,7 @@
-#!/usr/bin/bash
-DOCKER_IMAGE=quay.io/pypa/manylinux2014_x86_64
-PLAT=manylinux2014_x86_64
-docker pull $DOCKER_IMAGE
+#!/bin/bash
+
+DOCKER_IMAGE=quay.io/pypa/manylinux2014_aarch64
+PLAT=manylinux2014_aarch64
+docker pull ${DOCKER_IMAGE}
 docker run --rm -e PLAT=$PLAT -v `pwd`:/io $DOCKER_IMAGE $PRE_CMD /io/scripts/build-wheels-pypi.sh
 ls wheelhouse/
