63074903fe6fcc6615ec97848a455018fae588f0
Satvik Dixit <78974280+satvik-dixit@users.noreply.github.com>
2021-06-18 00:24:07 +0530

    Update test_angle_function.py

--- pyroomacoustics/tests/test_angle_function.py
+++ pyroomacoustics/tests/test_angle_function.py
@@ -1,38 +1,50 @@
 import numpy as np
-
-A=np.array([[0,0,0],[0,0,1],[1,1,1]])       # set of multiple points in 3-D
-X=np.array([[0,0,0]])                       # set of 1 point in 3-D
-
-B=np.array([0,0,1])                         # point in 3-D
-C=np.array([0,0,-1])                        # point in 3-D
-
-A1=np.array([[0,0],[0,1]])              # set of multiple points in 2-D
-X1=np.array([[1,1]])                    # set of only 1 point in 2-D
-
-B1=np.array([0,0])                      # point in 2-D
-C1=np.array([0,-1])                     # point in 2-D
+from pyroomacoustics import angle_function
+import unittest 
 
 
-print("angle function for points A and B returns: ")
-print(angle_function(A,B))
+pi=np.pi
 
-print("angle function for points A and C returns: ")
-print(angle_function(A,C))
+a1=np.array([[0,0,0],[0,0,1],[1,1,1]]) 
+a2=np.array([[0,0,1],[0,0,1]])
+a3=np.array([[0,0,1]])
+    
+b1=np.array([0,0,0])
+b2=np.array([1,-1,1])
+b3=np.array([1,0,1])                        
 
-print("angle function for points X and B returns: ")
-print(angle_function(X,B))
+c1=np.array([[0,0],[0,0]])
+c2=np.array([[1,0],[0,-1],[0,0]])
+c3=np.array([[1,1]])
 
-print("angle function for points X and C returns: ")
-print(angle_function(X,C))
+d1=np.array([0,0])
+d2=np.array([0,1])
+d3=np.array([1,-1])
 
-print("angle function for points A1 and B1 returns: ")
-print(angle_function(A1,B1))
 
-print("angle function for points A1 and C1 returns: ")
-print(angle_function(A1,C1))
+class TestAngleFunction(unittest.TestCase):
+    def test_angle_function(self):
 
-print("angle function for points X1 and B1 returns: ")
-print(angle_function(X1,B1))
+        self.assertTrue(angle_function(a1,b1).all()==np.array([[0,0],[0,pi/2],[pi/4,pi/4]]).all())
+        self.assertTrue(angle_function(a1,b2).all()==np.array([[3*pi/4,-pi/4],[3*pi/4,0],[pi/2,0]]).all())
+        self.assertTrue(angle_function(a1,b3).all()==np.array([[pi,-1.10714872],[pi,0],[pi/2,0]]).all())
+        self.assertTrue(angle_function(a2,b1).all()==np.array([[0,pi/2],[0,pi/2]]).all())
+        self.assertTrue(angle_function(a2,b2).all()==np.array([[3*pi/4,0],[3*pi/4,0]]).all())
+        self.assertTrue(angle_function(a2,b3).all()==np.array([[pi,0],[pi,0]]).all())
+        self.assertTrue(angle_function(a3,b1).all()==np.array([[0,pi/2]]).all())
+        self.assertTrue(angle_function(a3,b2).all()==np.array([[3*pi/4,0]]).all())
+        self.assertTrue(angle_function(a3,b3).all()==np.array([[pi,0]]).all())
 
-print("angle function for points X1 and C1 returns: ")
-print(angle_function(X1,C1))
+        self.assertTrue(angle_function(c1,d1).all()==np.array([[0,0],[0,0]]).all())
+        self.assertTrue(angle_function(c1,d2).all()==np.array([[-pi/2,0],[-pi/2,0]]).all())
+        self.assertTrue(angle_function(c1,d3).all()==np.array([[3*pi/4,0],[3*pi/4,0]]).all())
+        self.assertTrue(angle_function(c2,d1).all()==np.array([[0,0],[-pi/2,0],[pi/4,0]]).all())
+        self.assertTrue(angle_function(c2,d2).all()==np.array([[-pi/4,0],[-pi/2,0],[-pi/2,0]]).all())
+        self.assertTrue(angle_function(c2,d3).all()==np.array([[pi/2,0],[pi,0],[3*pi/4,0]]).all())
+        self.assertTrue(angle_function(c3,d1).all()==np.array([[pi/4,0]]).all())
+        self.assertTrue(angle_function(c3,d2).all()==np.array([[0,0]]).all())
+        self.assertTrue(angle_function(c3,d3).all()==np.array([[pi/2,0]]).all())
+
+
+if __name__=='__main__':
+    unittest.main()
