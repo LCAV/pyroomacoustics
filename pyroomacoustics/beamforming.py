9587ea167de5391c345435e63c6d7059165e3098
Satvik Dixit <78974280+satvik-dixit@users.noreply.github.com>
2021-07-07 02:14:22 +0530

    added set_directivity function

--- pyroomacoustics/beamforming.py
+++ pyroomacoustics/beamforming.py
@@ -307,12 +307,37 @@ class MicrophoneArray(object):
         self.directivity = None
         
         if directivity is not None:
-            raise NotImplementedError
+            set_directivity(directivity, n_mics) # need to add `n_mics` here
             
         self.signals = None
 
         self.center = np.mean(R, axis=1, keepdims=True)
+        
+    def set_directivity(directivities, n_mics):
 
+    """
+    This functions sets self.directivity as a list of directivities with `n_mics` entries, 
+    where `n_mics` is the number of microphones
+
+    Parameters
+    -----------
+
+    directivities:
+        single directivity for all microphones or a list of directivities for each microphone
+    n_mics
+        number of microphones
+    """
+    
+    if isinstance(directivities, np.ndarray):
+        
+        # list of directivities specified
+        assert directivities.shape[0] == n_mics
+        self.directivity = directivities
+    else:
+        
+        # only 1 directivity specified
+        self.directivity = np.ones(n_mics)*directivities
+        
     def record(self, signals, fs):
         """
         This simulates the recording of the signals by the microphones.
