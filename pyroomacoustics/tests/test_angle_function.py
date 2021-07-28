6866321187083bd2b670aefbedab22f6fdb08108
Satvik Dixit <78974280+satvik-dixit@users.noreply.github.com>
2021-07-28 12:44:08 +0530

    circulr helper function

--- pyroomacoustics/tests/test_angle_function.py
+++ pyroomacoustics/tests/test_angle_function.py
@@ -7,37 +7,37 @@ from unittest import TestCase
 pi = np.pi
 
 # for 3-D coordinates
-a1 = np.array([[0,0,0],[0,0,1],[1,1,1]]) 
-a2 = np.array([[0,0,1]])
-a3 = np.array([0,0,1])
+a1 = np.array([[0,0,1],[0,0,1],[0,1,1]]) 
+a2 = np.array([[0],[0],[1]])
+a3 = np.array([0,0,1]).T
     
-b1 = np.array([0,0,0])
-b2 = np.array([1,-1,1])
+b1 = np.array([[0],[0],[0]])
+b2 = np.array([1,-1,1]).T
                         
 
-a1_b1 = np.array([[0,0],[0,0],[pi/4,pi/4]])
-a1_b2 = np.array([[3*pi/4,3*pi/4],[3*pi/4,pi/2],[pi/2,pi/2]])
-a2_b1 = np.array([[0,0]])
-a2_b2 = np.array([[3*pi/4,pi/2]])
-a3_b1 = np.array([[0,0]])
-a3_b2 = np.array([[3*pi/4,pi/2]])
+a1_b1 = np.array([[0,0,pi/4],[0,0,pi/4]])
+a1_b2 = np.array([[3*pi/4,3*pi/4,pi/2],[3*pi/4,pi/2,pi/2]])
+a2_b1 = np.array([[0],[0]])
+a2_b2 = np.array([[3*pi/4],[pi/2]])
+a3_b1 = np.array([[0],[0]])
+a3_b2 = np.array([[3*pi/4],[pi/2]])
 
 
 # for 2-D coordinates
 c1 = np.array([[0,0],[0,0]])
-c2 = np.array([[1,1]])
-c3 = np.array([1,1])
+c2 = np.array([[1],[1]])
+c3 = np.array([1,1]).T
 
-d1 = np.array([0,0])
-d2 = np.array([0,1])
+d1 = np.array([[0],[0]])
+d2 = np.array([0,1]).T
 
 
-c1_d1 = np.array([[0,pi/2],[0,pi/2]])
-c1_d2 = np.array([[-pi/2,pi/2],[-pi/2,pi/2]])
-c2_d1 = np.array([[pi/4,pi/2]])
-c2_d2 = np.array([[0,pi/2]])
-c3_d1 = np.array([[pi/4,pi/2]])
-c3_d2 = np.array([[0,pi/2]])
+c1_d1 = np.array([[0,0],[pi/2,pi/2]])
+c1_d2 = np.array([[-pi/2,-pi/2],[pi/2,pi/2]])
+c2_d1 = np.array([[pi/4],[pi/2]])
+c2_d2 = np.array([[0],[pi/2]])
+c3_d1 = np.array([[pi/4],[pi/2]])
+c3_d2 = np.array([[0],[pi/2]])
 
 
 class TestAngleFunction(TestCase):
@@ -81,11 +81,12 @@ def find_error(type_coordinates):
         for a in a_range:
             for b in b_range:
 
-                error_azimuth = (angle_function(a,b) - a_b_range[a_b_index])[:,0]
-                error_colatitude = (angle_function(a,b) - a_b_range[a_b_index])[:,1]
+                error_azimuth = (angle_function(a,b) - a_b_range[a_b_index])[0]
+                error_colatitude = (angle_function(a,b) - a_b_range[a_b_index])[1]
 
                 print("for points :")
-                print(a,b)
+                print(a)
+                print(b)
                 print("error in azimuth calculation: {}".format(np.average(error_azimuth)))
                 print("error in colatitude calculation: {}".format(np.average(error_colatitude)))
                 print()
@@ -104,11 +105,12 @@ def find_error(type_coordinates):
         for c in c_range:
             for d in d_range:
 
-                error_azimuth = (angle_function(c,d) - c_d_range[c_d_index])[:,0]
-                error_colatitude = (angle_function(c,d) - c_d_range[c_d_index])[:,1]
+                error_azimuth = (angle_function(c,d) - c_d_range[c_d_index])[0]
+                error_colatitude = (angle_function(c,d) - c_d_range[c_d_index])[1]
 
                 print("for points :")
-                print(c,d)
+                print(c)
+                print(d)
                 print("error in azimuth calculation: {}".format(np.average(error_azimuth)))
                 print("error in colatitude calculation: {}".format(np.average(error_colatitude)))
                 print()
@@ -119,4 +121,3 @@ def find_error(type_coordinates):
 if __name__ == '__main__':
     find_error('3-D')
     find_error('2-D')
-
