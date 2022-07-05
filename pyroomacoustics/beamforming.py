33037a88b8acc3bf4e101b3e9326f9ed70939ef5
Malinda <malinda.dilhara@gmail.com>
2022-07-04 18:06:10 -0700

    use np.linalg.multi_dot

--- pyroomacoustics/beamforming.py
+++ pyroomacoustics/beamforming.py
@@ -1505,6 +1505,6 @@ class Beamformer(MicrophoneArray):
         # compute and return SNR
         A = np.dot(g_val.T, H[:, :L])
         num = np.dot(A, A.T)
-        denom = np.dot(np.dot(g_val.T, K_nq), g_val)
+        denom = np.linalg.multi_dot(g_val.T, K_nq, g_val)
 
         return num / denom
