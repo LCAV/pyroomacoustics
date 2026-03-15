1. Re-factor the directivities to accept cartesian coordinate inputs too for the response. 2. Fixes the scaling of the rays' energies in sample_rays.

pyroomacoustics/directivities/analytic.py
@@ -65,6 +65,7 @@ from ..doa import cart2spher, fibonacci_spherical_sampling, spher2cart
 from ..utilities import all_combinations, requires_matplotlib
 from .base import Directivity
 from .direction import DirectionVector
+from .integration import robust_spherical_voronoi_areas
 
 _FIGURE_EIGHT = 0
 _HYPERCARDIOID = 0.25
@@ -164,11 +165,40 @@ class CardioidFamily(Directivity):
         assert isinstance(orientation, DirectionVector)
         self._orientation = orientation
 
-    def _get_response_from_cartesian_vector(self, vectors):
+    def _get_response_from_cartesian_vector(self, vectors, magnitude):
         """vectors.shape == (3, n_points)."""
         return self._gain * (
             self._p + (1 - self._p) * np.matmul(self._orientation.unit_vector, vectors)
         )
+        cos_angle = np.matmul(self._orientation.unit_vector, vectors)
+        resp = self._gain * (self._p + (1 - self._p) * cos_angle)
+        if magnitude:
+            resp = np.abs(resp)
+        return resp
+
+    def get_response_cartesian(self, directions, magnitude=False, frequency=None):
+        """
+        Get response for provided direction cartesian vectors.
+
+        Parameters
+        ----------
+        cartesian: np.ndarray, (n_points, 3)
+            The direction of the desired responses
+        magnitude: bool
+            Ignored
+        frequency: np.ndarray, (n_freq,)
+            Ignored
+
+        Returns
+        -------
+        resp : :py:class:`~numpy.ndarray`
+            Response at provided directions.
+        """
+        if self._p == 1.0:
+            return self._gain * np.ones(directions.shape[0])
+        # Normalize.
+        directions = directions / np.linalg.norm(directions, axis=1, keepdims=True)
+        return self._get_response_from_cartesian_vector(directions.T, magnitude)
 
     def get_response(
         self, azimuth, colatitude=None, magnitude=False, frequency=None, degrees=True
@@ -199,27 +229,24 @@ class CardioidFamily(Directivity):
 
         if colatitude is not None:
             assert len(azimuth) == len(colatitude)
+
         if self._p == 1.0:
             return self._gain * np.ones(len(azimuth))
-        else:
-            coord = spher2cart(azimuth=azimuth, colatitude=colatitude, degrees=degrees)
 
-            resp = self._get_response_from_cartesian_vector(coord)
+        coord = spher2cart(azimuth=azimuth, colatitude=colatitude, degrees=degrees)
 
-            if magnitude:
-                return np.abs(resp)
-            else:
-                return resp
+        return self._get_response_from_cartesian_vector(coord, magnitude)
 
     def sample_rays(self, n_rays, rng=None):
         if rng is None:
             rays = fibonacci_spherical_sampling(n_points=n_rays).T
-            energies = (
-                self._get_response_from_cartesian_vector(rays.T) ** 2 * 4.0 * np.pi
-            )
+            resp = self._get_response_from_cartesian_vector(rays.T, magnitude=False)
+            energies = resp**2
         else:
             rays = self.energy_distribution.sample(n_rays, rng=rng)
-            energies = np.ones(n_rays) * self.energy_distribution.total_energy
+            energies = (
+                np.ones(n_rays) * self.energy_distribution.total_energy / (4.0 * np.pi)
+            )
         # Cardioid have a single band.
         return rays, energies[:, np.newaxis]
 

pyroomacoustics/directivities/measured.py
@@ -174,6 +174,16 @@ class MeasuredDirectivity(Directivity):
         """Length of the impulse response in samples"""
         return self._irs.shape[-1]
 
+    def get_direction_vectors(self):
+        """
+        Get the direction vectors of the measurements.
+
+        Returns
+        -------
+        A (n_directions, 3) numpy ndarray of unit vectors.
+        """
+        return self._grid.cartesian.T
+
     def _compute_band_energies(self):
         # Use an energy preserving octave band to compute the per octave
         # band energy of the impulse responses.
@@ -186,7 +196,7 @@ class MeasuredDirectivity(Directivity):
         # Energies per direction and octave band, shape == (n_grid, n_bands).
         # Normalized per bandwidth and area.
         self.areas = robust_spherical_voronoi_areas(self._original_grid.cartesian.T)
-        norm = octave_bands.get_bw()
+        norm = octave_bands.get_bw() / np.sum(octave_bands.get_bw())
         self.energies = np.array([octave_bands.energy(ir) / norm for ir in self._irs])
 
         # The total energy per direction over all bands.
@@ -197,7 +207,7 @@ class MeasuredDirectivity(Directivity):
         self.total_energy = np.sum(self.energies * self.areas[:, None])
 
         # Ray energies to use with the deterministic sampler.
-        self.energies_coeff_deterministic = self.energies * 4.0 * np.pi
+        self.energies_coeff_deterministic = self.energies
 
         # Ray energies to use with the random sampler.
         norm = (self.energy_per_direction / self.total_energy)[:, None]
@@ -230,6 +240,27 @@ class MeasuredDirectivity(Directivity):
             self._kdtree, self.energy_per_direction, areas=self.areas
         )
 
+    def get_response_cartesian(self, directions, magnitude=False, frequency=None):
+        """
+        Get response for provided direction cartesian vectors.
+
+        Parameters
+        ----------
+        cartesian: np.ndarray, (n_points, 3)
+            The direction of the desired responses
+        magnitude: bool
+            Ignored
+        frequency: np.ndarray, (n_freq,)
+            Ignored
+
+        Returns
+        -------
+        resp : :py:class:`~numpy.ndarray`
+            Response at provided directions.
+        """
+        _, index = self._kdtree.query(directions)
+        return self._irs[index, :]
+
     def get_response(
         self, azimuth, colatitude=None, magnitude=False, frequency=None, degrees=True
     ):
@@ -255,8 +286,7 @@ class MeasuredDirectivity(Directivity):
 
         cart = spher2cart(azimuth, colatitude)
 
-        _, index = self._kdtree.query(cart.T)
-        return self._irs[index, :]
+        return self.get_response_cartesian(cart.T)
 
     def sample_rays(self, n_rays, rng=None):
         if rng is None:
@@ -264,7 +294,7 @@ class MeasuredDirectivity(Directivity):
             energies_coeff = self.energies_coeff_deterministic
         else:
             rays = self.energy_distribution.sample(size=n_rays, rng=rng)
-            energies_coeff = self.energies_coeff_random
+            energies_coeff = self.energies_coeff_random / (4.0 * np.pi)
 
         _, index = self._kdtree.query(rays)
         energies = energies_coeff[index, :]

pyroomacoustics/directivities/tests/test_cardioid_energy_distribution.py
@@ -1,6 +1,7 @@
 import numpy as np
-import pyroomacoustics as pra
 import pytest
+
+import pyroomacoustics as pra
 
 
 @pytest.mark.parametrize("p", (0.0, 0.25, 0.5, 0.75, 1.0))
@@ -76,9 +77,9 @@ def test_sample_rays(p, gain, rng, rtol):
 
     dir_obj = pra.directivities.CardioidFamily(loc, p, gain=gain)
 
+    # Monte-Carlo integral: dOmega = 4.0 * np.pi / n_rays.
     rays, energies = dir_obj.sample_rays(100_000, rng=rng)
-
-    measured_total_energy = np.mean(energies)
+    measured_total_energy = 4.0 * np.pi * np.mean(energies)
 
     # Expected quantities.
     expected_total_energy = dir_obj.energy_distribution.total_energy

pyroomacoustics/directivities/tests/test_measured_energy_distribution.py
@@ -1,6 +1,7 @@
 import numpy as np
-import pyroomacoustics as pra
 import pytest
+
+import pyroomacoustics as pra
 
 
 def test_robust_spherical_voronoi_areas_1point():
@@ -145,9 +146,10 @@ def test_sample_rays(rng, rtol):
     for i in range(kdtree.n):
         nz = index == i
         y = energies[nz, :]
-        measured_energies[i, :] += np.sum(y, axis=0) / rays.shape[0]
+        measured_energies[i, :] += 4.0 * np.pi * np.sum(y, axis=0) / rays.shape[0]
 
-    measured_energy_band = np.mean(energies, axis=0)
+    # Monte-Carlo integral: dOmega = 4.0 * np.pi / n_rays.
+    measured_energy_band = 4.0 * np.pi * np.mean(energies, axis=0)
     measured_total_energy = np.sum(measured_energy_band)
 
     # Expected quantities.
@@ -159,7 +161,7 @@ def test_sample_rays(rng, rtol):
     np.testing.assert_allclose(expected_energy_band, measured_energy_band, atol=0.01)
 
     np.testing.assert_allclose(
-        expected_energies, measured_energies, atol=1e-5, rtol=rtol
+        expected_energies, measured_energies, atol=1e-2, rtol=rtol
     )
 
 